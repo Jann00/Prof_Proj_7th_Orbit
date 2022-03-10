8cfc7de5e3caf087a11bf2fc52fc11a7815e1966
laurence-dorman <laurencedorman131@gmail.com>
2022-03-10 15:20:36 +0000

    we can just move include folder also

--- get_wwise_libs.bat
+++ get_wwise_libs.bat
@@ -2,7 +2,7 @@
 cd /d %~dp0
 :: Check for Wwise install, so we can move the files instead of downloading.
 if exist "C:\Program Files (x86)\Audiokinetic\Wwise 2021.1.6.7774\" (
-	robocopy "C:\Program Files (x86)\Audiokinetic\Wwise 2021.1.6.7774\SDK" Plugins\Wwise\ThirdParty /e /xd "C:\Program Files (x86)\Audiokinetic\Wwise 2021.1.6.7774\SDK\source" "C:\Program Files (x86)\Audiokinetic\Wwise 2021.1.6.7774\SDK\samples" "C:\Program Files (x86)\Audiokinetic\Wwise 2021.1.6.7774\SDK\include" "C:\Program Files (x86)\Audiokinetic\Wwise 2021.1.6.7774\SDK\Help" 
+	robocopy "C:\Program Files (x86)\Audiokinetic\Wwise 2021.1.6.7774\SDK" Plugins\Wwise\ThirdParty /e /xd "C:\Program Files (x86)\Audiokinetic\Wwise 2021.1.6.7774\SDK\source" "C:\Program Files (x86)\Audiokinetic\Wwise 2021.1.6.7774\SDK\samples" "C:\Program Files (x86)\Audiokinetic\Wwise 2021.1.6.7774\SDK\Help" 
 	pause
 	goto :eof
 )
