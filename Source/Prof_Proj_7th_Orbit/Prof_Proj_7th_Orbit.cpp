8b826e3d15b784860dbcf2b6a5b794532e382ee1
Jann00 <61366436+Jann00@users.noreply.github.com>
2022-03-15 12:17:05 +0000

    merge

---
+++ Source/Prof_Proj_7th_Orbit/Prof_Proj_7th_Orbit.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Prof_Proj_7th_Orbit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Prof_Proj_7th_Orbit, "Prof_Proj_7th_Orbit" );
